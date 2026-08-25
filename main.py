752830e8b7f026444dc84be90d7403cf0cee57c8
qwerty0098 <118968642+qwerty0098@users.noreply.github.com>
2026-08-25 23:49:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-1
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return """
+    <h1>Arbitrage Scanner</h1>
+    <p>Сервис успешно запущен на Render 🚀</p>
+    """
