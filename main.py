4d6854fd9aab1a00978bdde5f16f12cfc7b4cb6c
qwerty0098 <118968642+qwerty0098@users.noreply.github.com>
2026-08-26 00:01:39 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify
 import ccxt
 
@@ -74,4 +75,5 @@ def scan():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
